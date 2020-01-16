44a6ea3bcf3cdaff1c318be589f75bc96f47a2a2
liko <15010343670@163.com>
2020-01-16 17:40:20 +0800

    swagger 集成

--- simple-development-demo/src/main/java/com/spring/simple/development/demo/service/impl/DemoBoImpl.java
+++ simple-development-demo/src/main/java/com/spring/simple/development/demo/service/impl/DemoBoImpl.java
@@ -2,6 +2,9 @@ package com.spring.simple.development.demo.service.impl;
 
 import com.alibaba.lava.base.AbstractLavaBoImpl;
 import com.spring.simple.development.core.annotation.base.IsApiService;
+import com.spring.simple.development.core.annotation.base.NoApiMethod;
+import com.spring.simple.development.core.component.mvc.req.ReqBody;
+import com.spring.simple.development.core.component.mvc.res.ResBody;
 import com.spring.simple.development.demo.mapper.DemoDoMapperExt;
 import com.spring.simple.development.demo.model.DemoDo;
 import com.spring.simple.development.demo.model.DemoDoExample;
@@ -9,12 +12,28 @@ import com.spring.simple.development.demo.service.DemoBo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.lang.reflect.Method;
+
 @Service
 @IsApiService()
 public class DemoBoImpl extends AbstractLavaBoImpl<DemoDo, DemoDoMapperExt, DemoDoExample> implements DemoBo {
 
     @Autowired
+    @NoApiMethod
     public void setBaseMapper(DemoDoMapperExt mapper) {
         setMapper(mapper);
     }
+
+    @Override
+    public ResBody queryDemo(ReqBody reqBody) {
+        return null;
+    }
+
+
+    @Override
+    public ResBody queryDemo2(ReqBody reqBody) {
+        return null;
+    }
+
+
 }
