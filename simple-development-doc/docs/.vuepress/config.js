03ec75fc1f1512e4b1daf43c6be94c3c640128f7
liko <15010343670@163.com>
2020-03-08 11:56:58 +0800

    文档

--- simple-development-doc/docs/.vuepress/config.js
+++ simple-development-doc/docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
     description: 'simple spring中文文档',
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
-        ['link', { rel: 'icon', href: '/favicon.ico' }], // 增加一个自定义的 favicon(网页标签的图标)
+        ['link', {rel: 'icon', href: '/favicon.ico'}], // 增加一个自定义的 favicon(网页标签的图标)
     ],
     markdown: {
         lineNumbers: true // 代码块显示行号
@@ -18,24 +18,37 @@ module.exports = {
         ],
         sidebar: [
             {
-                title: '开发指南',
-                path: '/simpleDoc/',
-                collapsable: false, //是否展开
+                title: '简介',
+                path: '/simpleDoc/simple/simple',
             },
-            ['./simpleDoc/install/install','安装'],
-            ['./simpleDoc/started/started','快速上手'],
             {
-                title: '组件',
+                title: '环境',
+                path: '/simpleDoc/environment/environment',
+            },
+            {
+                title: '快速入门',
                 sidebarDepth: 1,    // 可选的, 默认值是 1
+                path: "/simpleDoc/started/started",
                 children: [
-                    {
-                        title: '开发指南',
-                        path: '/simpleDoc/',
-                        collapsable: false, //是否展开
-                    }
+                    {title: "安装", path: "/simpleDoc/started/install/install"}
                 ]
             },
-            ['./simpleDoc/icon/icon','icon'],
+            {
+                title: '整体设计',
+                path: '/simpleDoc/design/design',
+            },
+            {
+                title: '基础组件',
+                path: '/simpleDoc/baseComponent/baseComponent',
+            },
+            {
+                title: '高级组件',
+                path: '/simpleDoc/advancedComponent/advancedComponent',
+            },
+            {
+                title: '关于我们',
+                path: '/simpleDoc/aboutus/aboutus',
+            }
         ]
     }
 }
