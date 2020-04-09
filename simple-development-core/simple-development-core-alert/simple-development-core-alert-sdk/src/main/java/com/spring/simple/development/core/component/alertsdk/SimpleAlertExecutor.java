5b46a103706a9be5144ee93c7aa6490f81cd291a
liko <15010343670@163.com>
2020-04-09 11:28:55 +0800

    bug fixed

--- simple-development-core/simple-development-core-alert/simple-development-core-alert-sdk/src/main/java/com/spring/simple/development/core/component/alertsdk/SimpleAlertExecutor.java
+++ simple-development-core/simple-development-core-alert/simple-development-core-alert-sdk/src/main/java/com/spring/simple/development/core/component/alertsdk/SimpleAlertExecutor.java
@@ -1,6 +1,13 @@
 package com.spring.simple.development.core.component.alertsdk;
 
 import com.spring.simple.development.core.component.alertsdk.handler.SimpleAlertConfig;
+import com.spring.simple.development.support.utils.DateUtils;
+import com.spring.simple.development.support.utils.IpUtil;
+import com.spring.simple.development.support.utils.PrimaryKeyGenerator;
+import com.spring.simple.development.support.utils.log.ErrorMessage;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 /**
  * @author liko.wang
@@ -44,6 +51,65 @@ public class SimpleAlertExecutor {
      **/
     public static void sendHighMessage(String message) {
         SimpleAlertConfig.putMessage(message, highMessage);
+    }
+
+    /**
+     * @param description
+     * @param e
+     * @return void
+     * @author liko.wang
+     * @Date 2020/3/31/031 16:28
+     * @Description 发送一般消息
+     **/
+    public static void sendThrowableLowMessage(String description, Throwable e) {
+        ErrorMessage errorMessage = getErrorMessage(description, e);
+        SimpleAlertConfig.putMessage(errorMessage.toString(), lowMessage);
+    }
+
+    private static ErrorMessage getErrorMessage(String description, Throwable e) {
+        // 日志转转
+        StringWriter stringWriter = new StringWriter();
+        e.printStackTrace(new PrintWriter(stringWriter));
+        String errorMsg = stringWriter.toString();
+        // 记录日志内容
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setLogId(PrimaryKeyGenerator.getInstance().nextId().toString());
+        errorMessage.setProjectType("api");
+        errorMessage.setLogType("restful api");
+        errorMessage.setIp(IpUtil.getIp());
+        errorMessage.setLogPath("/data/logs/simple-development-core/error/error.log");
+        errorMessage.setDate(DateUtils.getCurrentTime());
+        errorMessage.setContent(errorMsg);
+        errorMessage.setDescription(description);
+        errorMessage.setUrl("");
+        errorMessage.setRemoteIp("");
+        return errorMessage;
+    }
+
+    /**
+     * @param description
+     * @param e
+     * @return void
+     * @author liko.wang
+     * @Date 2020/3/31/031 16:28
+     * @Description 发送重要消息
+     **/
+    public static void sendThrowableMediumMessage(String description, Throwable e) {
+        ErrorMessage errorMessage = getErrorMessage(description, e);
+        SimpleAlertConfig.putMessage(errorMessage.toString(), mediumMessage);
+    }
+
+    /**
+     * @param description
+     * @param e
+     * @return void
+     * @author liko.wang
+     * @Date 2020/3/31/031 16:29
+     * @Description 发送特别重要消息
+     **/
+    public static void sendThrowableHighMessage(String description, Throwable e) {
+        ErrorMessage errorMessage = getErrorMessage(description, e);
+        SimpleAlertConfig.putMessage(errorMessage.toString(), highMessage);
     }
 
 }
