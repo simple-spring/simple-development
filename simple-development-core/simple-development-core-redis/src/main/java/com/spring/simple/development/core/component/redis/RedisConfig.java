878825446c06a39e9a969e59ee8eee35b5ffba07
root <restart@2020>
2020-05-20 16:02:48 +0800

    GC问题 后续跟进

--- simple-development-core/simple-development-core-redis/src/main/java/com/spring/simple/development/core/component/redis/RedisConfig.java
+++ simple-development-core/simple-development-core-redis/src/main/java/com/spring/simple/development/core/component/redis/RedisConfig.java
@@ -1,24 +1,12 @@
 package com.spring.simple.development.core.component.redis;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.simple.development.support.properties.PropertyConfigurer;
 import com.spring.simple.development.support.utils.JedisPoolUtils;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.util.Assert;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
-
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * redis组件
