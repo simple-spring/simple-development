8a4bdb6fd50648d5ae2993aa4b4e740e4ca06639
liko <15010343670@163.com>
2020-06-21 23:15:41 +0800

    数据转换工具

--- simple-development-core/simple-development-core-data-process/src/main/java/com/spring/simple/development/core/component/data/process/executor/mapper/SimpleMapper.java
+++ simple-development-core/simple-development-core-data-process/src/main/java/com/spring/simple/development/core/component/data/process/executor/mapper/SimpleMapper.java
@@ -1,7 +1,9 @@
 package com.spring.simple.development.core.component.data.process.executor.mapper;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -10,11 +12,30 @@ import java.util.List;
  */
 public interface SimpleMapper {
     /**
-     * 执行sql
+     * 执行查询sql
      *
      * @param sql
      * @return
      */
     @Select("${sql}")
     List<Object> executorSql(@Param("sql") String sql, @Param("resultType") Class resultType);
+
+    /**
+     * 执行更新and删除sql
+     *
+     * @param sql
+     * @return
+     */
+    @Update("${sql}")
+    Integer updateSql(@Param("sql") String sql);
+
+    /**
+     * 执行新增sql
+     *
+     * @param sql
+     * @return
+     */
+    @Insert("${sql}")
+    Integer insertSql(@Param("sql") String sql);
+
 }
