27c11bd0eb1a6ba366f486aae75e53987d25a422
root <restart@2020>
2020-06-28 15:48:00 +0800

    init

--- simple-development-demo/src/main/java/com/spring/simple/development/demo/controller/DemoController.java
+++ simple-development-demo/src/main/java/com/spring/simple/development/demo/controller/DemoController.java
@@ -4,6 +4,8 @@ import com.acl.support.auth.web.util.AccountUtils;
 import com.acl.xauth.anno.authc.NoAuth;
 import com.alibaba.lava.privilege.PrivilegeInfo;
 import com.spring.simple.development.demo.service.TestDemoBo;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,28 +23,37 @@ import java.io.IOException;
  **/
 @Controller
 public class DemoController {
+    private static final Logger logger = LogManager.getLogger(DemoController.class);
+
+
     @Autowired
     private TestDemoBo testDemoBo;
     @Autowired
     PrivilegeInfo privilegeInfo;
-//    @RequestMapping("/")
+
+    //    @RequestMapping("/")
 //    @ResponseBody
 //    public String home(HttpServletRequest request, HttpServletResponse response) throws IOException {
 //        return "hello world";
 //    }
     @NoAuth
+    @ResponseBody
     @RequestMapping("/index")
-    public String index(HttpServletRequest request,HttpServletResponse response) {
-       AccountUtils.getCurrentAccount();
-        return AccountUtils.getCurrentAccount().toString();
+    public String index(HttpServletRequest request, HttpServletResponse response) {
+        logger.info("123");
+        logger.warn("456");
+        logger.error("567");
+        return "index";
     }
+
     @NoAuth
     @RequestMapping("/noPermission")
     public String noPermission() {
         return "noPermission";
     }
+
     @RequestMapping("/testPermission")
-    public String testPermission(HttpServletRequest request,HttpServletResponse response) throws IOException {
+    public String testPermission(HttpServletRequest request, HttpServletResponse response) throws IOException {
         testDemoBo.getData("1");
         return "testPermission";
     }
